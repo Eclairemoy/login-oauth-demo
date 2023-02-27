b80f864ade5ecdcd3c9538caf41033fb6aa38635
Liz Moy <lizmoy@Lizs-MacBook-Pro.local>
2023-02-27 08:31:42 +0000

    updating form

--- pages/FormComponent.js
+++ pages/FormComponent.js
@@ -1,31 +1,46 @@
+import { useEvervault } from '@evervault/react';
+import { createClient } from '@supabase/supabase-js'
+
 export default function FormComponent() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    const evervault = useEvervault();
     
+    const encryptedPassword = await evervault.encrypt(event.target.password.value);
     const customerName = event.target.name.value;
     const customerEmail = event.target.email.value;
 
     // Send the data to the server in JSON format.
-    const JSONdata = JSON.stringify({'name': customerName, 'email': customerEmail})
-    const endpoint = '/api/savetodb'
+    const JSONdata = JSON.stringify({'name': customerName, 'email': customerEmail, 'password': encryptedPassword})
 
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    }
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+    const dbData = await supabase.from('users').insert(JSONdata).then(response => { 
+        return response
+    })
+    .catch(function (error) {
+        return error
+    })
+    console.log({'status': dbData.statusText, 'data': JSONdata}) 
+    // const endpoint = '/api/savetodb'
 
-    // Send the form data to our forms API on Vercel and get a response.
-    const response = await fetch(endpoint, options)
+    // const options = {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   // Body of the request is the JSON data we created above.
+    //   body: JSONdata,
+    // }
 
-    // Get the response data from server as JSON.
-    const result = await response.json()
-    console.log(result.data)
-    alert(`Password saved: ${result.data}`)
+    // // Send the form data to our forms API on Vercel and get a response.
+    // const response = await fetch(endpoint, options)
+
+    // // Get the response data from server as JSON.
+    // const result = await response.json()
+    // console.log(result.data)
+    alert(`Password saved: ${encryptedPassword}`)
 
   };
 
